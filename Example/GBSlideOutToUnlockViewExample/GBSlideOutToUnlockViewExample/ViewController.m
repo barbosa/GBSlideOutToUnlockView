fdeebc902ac00fc7a18d3fed57283a9a70a3268f
Gustavo Barbosa <gustavocsb@gmail.com>
2014-04-17 00:42:27 -0300

    Added an UILabel on example project to debug states.

--- Example/GBSlideOutToUnlockViewExample/GBSlideOutToUnlockViewExample/ViewController.m
+++ Example/GBSlideOutToUnlockViewExample/GBSlideOutToUnlockViewExample/ViewController.m
@@ -10,15 +10,38 @@
 #import "GBSlideOutToUnlockView.h"
 
 @interface ViewController ()<GBSlideOutToUnlockViewDelegate>
+{
+    UILabel *currentState;
+    GBSlideOutToUnlockView *slideToUnlockView;
+}
 @end
 
 @implementation ViewController
 
 - (void)loadView
 {
-    GBSlideOutToUnlockView *slideToUnlockView = [[GBSlideOutToUnlockView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    slideToUnlockView = [[GBSlideOutToUnlockView alloc] initWithFrame:[UIScreen mainScreen].bounds];
     slideToUnlockView.delegate = self;
+    slideToUnlockView.tintColor = [UIColor purpleColor];
     self.view = slideToUnlockView;
+}
+
+- (void)viewDidLoad
+{
+    currentState = [UILabel new];
+    currentState.text = @"Initial";
+    [currentState sizeToFit];
+    
+    [self setCurrentStateLabelPosition];
+    
+    [self.view addSubview:currentState];
+}
+
+- (void)setCurrentStateLabelPosition
+{
+    CGFloat x = self.view.center.x;
+    CGFloat y = CGRectGetHeight([UIScreen mainScreen].bounds) - CGRectGetHeight(currentState.frame);
+    currentState.center = CGPointMake(x, y);
 }
 
 #pragma mark - GBSlideOutToUnlockViewDelegate
@@ -26,16 +49,25 @@
 - (void)slideOutToUnlockViewDidStartToDrag:(GBSlideOutToUnlockView *)slideOutToUnlockView
 {
     NSLog(@"did start");
+    currentState.text = @"Started";
+    [currentState sizeToFit];
+    [self setCurrentStateLabelPosition];
 }
 
 - (void)slideOutToUnlockViewDidEndToDrag:(GBSlideOutToUnlockView *)slideOutToUnlockView
 {
     NSLog(@"did end");
+    currentState.text = @"Ended";
+    [currentState sizeToFit];
+    [self setCurrentStateLabelPosition];
 }
 
 - (void)slideOutToUnlockViewDidUnlock:(GBSlideOutToUnlockView *)slideOutToUnlockView
 {
     NSLog(@"Did unlock");
+    currentState.text = @"Unlocked";
+    [currentState sizeToFit];
+    [self setCurrentStateLabelPosition];
 }
 
 @end
