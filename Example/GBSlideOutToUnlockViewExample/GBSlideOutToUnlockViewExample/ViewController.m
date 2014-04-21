13fdb790807b27a5b6a824e8bfa01af89bda41af
Gustavo Barbosa <gustavocsb@gmail.com>
2014-04-21 12:51:39 -0300

    Update Example project.

--- Example/GBSlideOutToUnlockViewExample/GBSlideOutToUnlockViewExample/ViewController.m
+++ Example/GBSlideOutToUnlockViewExample/GBSlideOutToUnlockViewExample/ViewController.m
@@ -11,37 +11,23 @@
 
 @interface ViewController ()<GBSlideOutToUnlockViewDelegate>
 {
-    UILabel *currentState;
+    __weak IBOutlet UIView *containerView;
+    __weak IBOutlet UILabel *currentStateLabel;
     GBSlideOutToUnlockView *slideToUnlockView;
 }
 @end
 
 @implementation ViewController
 
-- (void)loadView
-{
-    slideToUnlockView = [[GBSlideOutToUnlockView alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    slideToUnlockView.delegate = self;
-    slideToUnlockView.tintColor = [UIColor purpleColor];
-    self.view = slideToUnlockView;
-}
-
 - (void)viewDidLoad
 {
-    currentState = [UILabel new];
-    currentState.text = @"Initial";
-    [currentState sizeToFit];
+    [super viewDidLoad];
     
-    [self setCurrentStateLabelPosition];
-    
-    [self.view addSubview:currentState];
-}
-
-- (void)setCurrentStateLabelPosition
-{
-    CGFloat x = self.view.center.x;
-    CGFloat y = CGRectGetHeight([UIScreen mainScreen].bounds) - CGRectGetHeight(currentState.frame);
-    currentState.center = CGPointMake(x, y);
+    slideToUnlockView = [[GBSlideOutToUnlockView alloc] initWithFrame:containerView.bounds];
+    slideToUnlockView.delegate = self;
+    slideToUnlockView.tintColor = [UIColor purpleColor];
+    slideToUnlockView.outerCircleRadius = CGRectGetWidth(containerView.bounds) / 2.0 - 2*20;
+    [containerView addSubview:slideToUnlockView];
 }
 
 #pragma mark - GBSlideOutToUnlockViewDelegate
@@ -49,25 +35,16 @@
 - (void)slideOutToUnlockViewDidStartToDrag:(GBSlideOutToUnlockView *)slideOutToUnlockView
 {
     NSLog(@"did start");
-    currentState.text = @"Started";
-    [currentState sizeToFit];
-    [self setCurrentStateLabelPosition];
 }
 
 - (void)slideOutToUnlockViewDidEndToDrag:(GBSlideOutToUnlockView *)slideOutToUnlockView
 {
     NSLog(@"did end");
-    currentState.text = @"Ended";
-    [currentState sizeToFit];
-    [self setCurrentStateLabelPosition];
 }
 
 - (void)slideOutToUnlockViewDidUnlock:(GBSlideOutToUnlockView *)slideOutToUnlockView
 {
     NSLog(@"Did unlock");
-    currentState.text = @"Unlocked";
-    [currentState sizeToFit];
-    [self setCurrentStateLabelPosition];
 }
 
 @end
