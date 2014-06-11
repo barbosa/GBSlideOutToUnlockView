cf9bf5510d27779711051477a29878b3cb546ad1
Gustavo Barbosa <gustavocsb@gmail.com>
2014-06-11 14:19:52 -0300

    Fix Example project podspec and workspace.

---
+++ Example/Pods/BuildHeaders/GBSlideOutToUnlockView/GBSlideOutToUnlockView.h
@@ -0,0 +1,1 @@
+../../../../Classes/GBSlideOutToUnlockView.h
---
+++ Example/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// GBSlideOutToUnlockView
+#define COCOAPODS_POD_AVAILABLE_GBSlideOutToUnlockView
+#define COCOAPODS_VERSION_MAJOR_GBSlideOutToUnlockView 1
+#define COCOAPODS_VERSION_MINOR_GBSlideOutToUnlockView 1
+#define COCOAPODS_VERSION_PATCH_GBSlideOutToUnlockView 0
+
