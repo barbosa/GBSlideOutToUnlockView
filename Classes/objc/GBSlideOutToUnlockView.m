502580335db020fd4b380aee32826c5e1abcb4c3
Gustavo Barbosa <gustavocsb@gmail.com>
2015-04-03 17:52:19 -0300

    update drag button after set its color

--- Classes/objc/GBSlideOutToUnlockView.m
+++ Classes/objc/GBSlideOutToUnlockView.m
@@ -20,7 +20,6 @@ static CGFloat const kDefaultInnerCircleRadius = 25.0f;
 
 @interface GBSlideOutToUnlockView ()
 {
-    BOOL _a;
     BOOL _unlockOnRelease;
     CGFloat _distanceMoved;
 }
@@ -32,6 +31,8 @@ static CGFloat const kDefaultInnerCircleRadius = 25.0f;
 
 
 @implementation GBSlideOutToUnlockView
+
+@synthesize draggableButtonBackgroundColor = _draggableButtonBackgroundColor;
 
 #pragma mark - Ctors
 
@@ -68,8 +69,6 @@ static CGFloat const kDefaultInnerCircleRadius = 25.0f;
 
 - (void)setupViews
 {
-    self.accessibilityLabel = @"unlock view";
-    
     self.backgroundColor = [UIColor clearColor];
     [self addRedeemImageAtCenter];
     [self addPanGestureRecognizerToImage];
@@ -91,10 +90,10 @@ static CGFloat const kDefaultInnerCircleRadius = 25.0f;
 {
     if (_outerCircleRadius)
         return _outerCircleRadius;
-    
+
     CGFloat horizontalSize = CGRectGetWidth(self.bounds);
     CGFloat verticalSize = CGRectGetHeight(self.bounds);
-    
+
     return MIN(horizontalSize, verticalSize) / 2.0f;
 }
 
@@ -111,6 +110,12 @@ static CGFloat const kDefaultInnerCircleRadius = 25.0f;
 - (UIColor *)draggableButtonBackgroundColor
 {
     return _draggableButtonBackgroundColor ?: self.tintColor;
+}
+
+- (void)setDraggableButtonBackgroundColor:(UIColor *)draggableButtonBackgroundColor
+{
+    _draggableButtonBackgroundColor = draggableButtonBackgroundColor;
+    _dragButton.backgroundColor = _draggableButtonBackgroundColor;
 }
 
 - (UIColor *)draggableImageTintColor
@@ -131,12 +136,12 @@ static CGFloat const kDefaultInnerCircleRadius = 25.0f;
     CGContextRef context = UIGraphicsGetCurrentContext();
     CGContextSetLineWidth(context, 1.0);
     CGContextSetStrokeColorWithColor(context, color.CGColor);
-    
+
     CGRect circle = CGRectMake(p_fixedCenter(self.frame).x - radius,
                                p_fixedCenter(self.frame).y - radius,
                                2 * radius,
                                2 * radius);
-    
+
     CGContextAddEllipseInRect(context, circle);
     CGContextStrokePath(context);
 }
@@ -144,18 +149,17 @@ static CGFloat const kDefaultInnerCircleRadius = 25.0f;
 - (void)addRedeemImageAtCenter
 {
     _dragButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    _dragButton.accessibilityLabel = @"drag button";
     _dragButton.frame = CGRectMake(0, 0, 2*self.innerCircleRadius, 2*self.innerCircleRadius);
     _dragButton.backgroundColor = self.draggableButtonBackgroundColor;
     _dragButton.tintColor = self.draggableImageTintColor;
     _dragButton.layer.cornerRadius = self.innerCircleRadius;
     _dragButton.clipsToBounds = YES;
-    
+
     UIImage *image = _draggableImage ?: [UIImage imageNamed:@"drag_button"];
     if ([image respondsToSelector:@selector(imageWithRenderingMode:)])
         image = [image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
     [_dragButton setImage:image forState:UIControlStateNormal];
-    
+
     [self addSubview:_dragButton];
 
     _dragButton.center = p_fixedCenter(self.frame);
@@ -178,24 +182,24 @@ static CGFloat const kDefaultInnerCircleRadius = 25.0f;
             [_delegate slideOutToUnlockViewDidStartToDrag:self];
         }
     }
-    
+
     if (gestureRecognizer.state == UIGestureRecognizerStateChanged) {
         CGPoint translation = [gestureRecognizer translationInView:self];
         _dragButton.center = CGPointMake(_fixedCenter.x + translation.x,
                                          _fixedCenter.y + translation.y);
         _distanceMoved = sqrt(pow(_dragButton.center.x-_fixedCenter.x, 2) + pow(_dragButton.center.y-_fixedCenter.y, 2));
-        
+
         if ([self.delegate respondsToSelector:@selector(slideOutToUnlockView:didDragDistance:)])
             [self.delegate slideOutToUnlockView:self didDragDistance:_distanceMoved];
-        
+
         _unlockOnRelease = _distanceMoved > self.outerCircleRadius + self.innerCircleRadius;
     }
-    
+
     if (gestureRecognizer.state == UIGestureRecognizerStateEnded) {
-        
+
         if ([self.delegate respondsToSelector:@selector(slideOutToUnlockViewDidEndToDrag:)])
             [self.delegate slideOutToUnlockViewDidEndToDrag:self];
-        
+
         if (_unlockOnRelease) {
             if ([self.delegate respondsToSelector:@selector(slideOutToUnlockViewDidUnlock:)])
                 [self.delegate slideOutToUnlockViewDidUnlock:self];
@@ -203,7 +207,7 @@ static CGFloat const kDefaultInnerCircleRadius = 25.0f;
             if ([self.delegate respondsToSelector:@selector(slideOutToUnlockViewDidNotUnlock:)])
                 [self.delegate slideOutToUnlockViewDidNotUnlock:self];
         }
-        
+
         [UIView animateWithDuration:0.2f animations:^{
             _dragButton.center = _fixedCenter;
         }];
